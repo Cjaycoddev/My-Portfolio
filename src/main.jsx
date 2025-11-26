f6becf762164eeaad8be2466cf005ed5d4a2a98b
Cjaydev <3589.2021@students.ku.ac.ke>
2025-11-27 01:24:43 +0300

    Fix: Render Vercel Analytics component to enable tracking

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/>
+    <Analytics/>
   </StrictMode>,
 )
